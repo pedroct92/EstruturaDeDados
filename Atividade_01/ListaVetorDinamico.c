995f4f210aa4808de8230f7760f2080c95e49685
Pedro Coelho Torres <pedroct92@gmail.com>
2013-01-03 20:32:32 -0200

    ListaDinamica

--- Atividade_01/ListaVetorDinamico.c
+++ Atividade_01/ListaVetorDinamico.c
@@ -3,113 +3,64 @@
 // ******************************************************************
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef enum { FALSE, TRUE } bool;
-
-typedef struct { float* V;
-               	 int fim;
-               	 int tamMax;
-                } Lista;
-
-// ***********************************************
-// ******  initLista                        ******
-// ***********************************************
-
-// ***********************************************
-// ******  limpaLista                       ******
-// ***********************************************
-
-
-// ***********************************************
-// ******  tamLista                         ******
-// ***********************************************
-
-
-// ***********************************************
-// ******  imprimeLista                     ******
-// ***********************************************
-
-
-// ***********************************************
-// ******  imprimeListaTotal                ******
-// ***********************************************
-
-
-// ***********************************************
-// ******  insereElemento                   ******
-// ***********************************************
-
-// ***********************************************
-// ******  buscaElemento                    ******
-// ***********************************************
-
-
-// ***********************************************
-// ******  removeElemento                   ******
-// ***********************************************
-
-
-// ***********************************************
-// ***********************************************
-// ***********************************************
+#include "ListaVetorDinamico.h"
 
 int main(int argc, char** argv) {
+    Lista L0, L1;
+    bool OK;
+    int i = 0, n;
 
-Lista L0, L1;
-bool OK;
-int i = 0, 
-	n;
+    initLista(&L0);
+    //imprimeLista(&L0);
+    //imprimeListaTotal(&L0);
 
-//    initLista L0 com 30 elementos;
-//    imprimeLista L0;
-//    imprimeListaTotal L0;
-    
-//    initLista L1 com 10 elementos;
-//    imprimeLista L1;
-//    imprimeListaTotal L1;
-    
-    do { 
+    initLista(&L1);
+    //imprimeLista(&L1);
+    //imprimeListaTotal(&L1);
+
+    do {
     	float n = rand() % 50;
-//    	OK = insereElemento em L0
+    	OK = insereElemento(&L0, n);
 		if (!OK)
     		break;
     	i++;
-    } while (i < 30);
+    } while (i < 5);
 
-    do { 
+    i = 0;
+    do {
     	float n = rand() % 50;
-//    	OK = insereElemento em L1
+    	OK = insereElemento(&L1, n);
 		if (!OK)
     		break;
     	i++;
     } while (i < 10);
-    
 
-//    	imprimeLista L0;
-//    	imprimeLista L1;
+   	imprimeLista(&L0);
+   	imprimeLista(&L1);
 
-//		n = tamLista L0;
+	n = tamLista(&L0);
 
-    for (i = 0 ; i < n ; i++)
-//		OK = removeElemento L0    	
-    	if (OK) 
+    for (i = 0 ; i < n ; i++){
+		OK = removeElemento(&L0, i);
+    	if (OK)
     		printf("Remocao %f OK\n", (float)i);
     	else
     		printf("Remocao %f nao OK\n", (float)i);
-    	
-//		n = tamLista L1;
+    }
 
-    for (i = 0 ; i < n ; i++)
-//		OK = removeElemento L1   	
-    	if (OK) 
+    n = tamLista(&L1);
+
+    for (i = 0 ; i < n ; i++){
+        OK = removeElemento(&L1, i);
+    	if (OK)
     		printf("Remocao %f OK\n", (float)i);
     	else
     		printf("Remocao %f nao OK\n", (float)i);
-    	
-//    imprimeLista L0;   
-//    imprimeLista L1;
+    }
 
+    imprimeLista(&L0);
+    imprimeLista(&L1);
+
+    imprimeListaTotal(&L0);
+    imprimeListaTotal(&L1);
 }
-
-    
-
---
+++ Atividade_01/ListaVetorDinamico.h
@@ -0,0 +1,144 @@
+#define MAX_LISTA 10
+
+typedef enum { FALSE, TRUE } bool;
+
+typedef struct { float dado;
+                 int   prox;
+               } tNo;
+
+typedef struct { tNo V[MAX_LISTA];
+                 int ini;
+                 int tm;
+               } Lista;
+
+int proxDisponivel(Lista *lst);
+int buscaAnterior(Lista *lst);
+
+// ***********************************************
+// ******  initLista                        ******
+// ***********************************************
+void initLista(Lista *lst){
+    limpaLista(lst);
+    lst->ini = -1;
+    lst->tm = 0;
+}
+// ***********************************************
+// ******  limpaLista                       ******
+// ***********************************************
+void limpaLista(Lista *lst){
+    int i;
+
+    for(i = 0; i < MAX_LISTA; i++){
+        lst->V[i].prox = i;
+    }
+}
+
+// ***********************************************
+// ******  tamLista                         ******
+// ***********************************************
+int tamLista(Lista *lst){
+    return lst->tm;
+}
+
+// ***********************************************
+// ******  imprimeLista                     ******
+// ***********************************************
+void imprimeLista(Lista *lst){
+    int i, pos;
+
+    pos = lst->ini;
+    if(pos != -1){
+        printf("%%%%%%%%%%%% INICIO LISTA %%%%%%%%%%%%%%\n");
+        while(pos != -1){
+            printf("[%f] [%d]\n", (float) lst->V[pos].dado, pos);
+            pos = lst->V[pos].prox;
+        }
+        printf("%%%%%%%%%%%% FIM LISTA %%%%%%%%%%%%%%%%%\n");
+    }
+}
+
+// ***********************************************
+// ******  imprimeListaTotal                ******
+// ***********************************************
+
+void imprimeListaTotal(Lista *lst){
+    int i;
+
+    printf("<<<<<<<< ------ Lista Total ----->>>>>>\n");
+    for(i = 0; i < MAX_LISTA; i++){
+        printf("Daddo[%f] Index[%i] Proximo[%d]\n",(float)lst->V[i].dado, (int) i, (int)lst->V[i].prox);
+    }
+}
+
+// ***********************************************
+// ******  insereElemento                   ******
+// ***********************************************
+
+bool insereElemento(Lista *lst,float var){
+    bool ok;
+    int pos, ant;
+
+    ant = buscaAnterior(lst);
+    pos = proxDisponivel(lst);
+
+    if(pos != -1){
+        if(lst->ini == -1)
+            lst->ini = pos;
+
+        lst->V[ant].prox = pos;
+        lst->V[pos].dado = var;
+        lst->V[pos].prox = -1;
+        lst->tm++;
+        ok = 1;
+    }
+    return ok;
+}
+
+// ***********************************************
+// ******  buscaElemento                    ******
+// ***********************************************
+int buscaElemento(Lista *lst, float var){
+    int i;
+    for(i = 0; i < MAX_LISTA; i++){
+        if(lst->V[i].dado == var)
+            return i;
+    }
+    return -1;
+}
+
+// ***********************************************
+// ******  removeElemento                   ******
+// ***********************************************
+bool removeElemento(Lista *lst, float var){
+    bool ok;
+    int pos;
+
+    pos = buscaElemento(lst, var);
+
+    if(pos != -1){
+
+    }
+    return ok;
+}
+
+int proxDisponivel(Lista *lst){
+    int i;
+    for(i = 0; i < MAX_LISTA; i++){
+        if(lst->V[i].prox == i)
+            return i;
+    }
+    return -1;
+}
+
+int buscaAnterior(Lista *lst){
+    int i;
+    for(i = 0; i < MAX_LISTA; i++){
+        if(lst->V[i].prox == -1)
+            return i;
+    }
+    return -1;
+}
+
+int buscaAnterior(){
+    return 1;
+}
